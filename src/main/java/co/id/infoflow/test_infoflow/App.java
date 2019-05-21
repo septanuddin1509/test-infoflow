ac0f2951aaa0c850142431ac4956438529c4fbf7
Septanuddin Satriatama <septa@infoflow.co.id>
2019-05-21 16:01:08 +0700

    initial project

---
+++ src/main/java/co/id/infoflow/test_infoflow/App.java
@@ -0,0 +1,13 @@
+package co.id.infoflow.test_infoflow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
